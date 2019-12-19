7d88f9e8b64bdcb2cc03ac504a5956e71228539e
Harshil Rathod <50371099+ImHarshil@users.noreply.github.com>
2019-12-19 20:51:53 +0530

    object detection added

--- app/src/main/java/com/example/harshil/facedetection/MainActivity.java
+++ app/src/main/java/com/example/harshil/facedetection/MainActivity.java
@@ -34,6 +34,11 @@ import com.google.android.gms.vision.face.Face;
 import com.google.android.gms.vision.face.FaceDetector;
 import com.google.firebase.ml.vision.FirebaseVision;
 import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
+import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
+import com.google.firebase.ml.vision.objects.FirebaseVisionObject;
+import com.google.firebase.ml.vision.objects.FirebaseVisionObjectDetector;
+import com.google.firebase.ml.vision.objects.FirebaseVisionObjectDetectorOptions;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
@@ -41,13 +46,13 @@ import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static final int CAMERA_PERMISSION_CODE = 100;
+    private static final int STORAGE_PERMISSION_CODE = 101;
     protected ImageView imageView;
     protected TextView textView;
     protected Button click;
     protected Button detect;
-    private static final int CAMERA_PERMISSION_CODE = 100;
-    private static final int STORAGE_PERMISSION_CODE = 101;
-
+    protected Button object;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +60,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         imageView = findViewById(R.id.imageView);
-        textView  = findViewById(R.id.textView);
-        click     = findViewById(R.id.buttonclick);
-        detect    = findViewById(R.id.buttondetect);
+        textView = findViewById(R.id.textView);
+        click = findViewById(R.id.buttonclick);
+        detect = findViewById(R.id.buttondetect);
+        object = findViewById(R.id.btnobject);
 
 
         detect.setOnClickListener(new View.OnClickListener() {
@@ -66,10 +72,9 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 try {
                     startActivityForResult(intent, 1);
-                }
-                catch (Exception e){
+                } catch (Exception e) {
                     textView.setText(e.toString());
-                    Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
                 }
 
             }
@@ -78,82 +83,100 @@ public class MainActivity extends AppCompatActivity {
         click.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                checkPermission(Manifest.permission.CAMERA,CAMERA_PERMISSION_CODE);
+                checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE);
 
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 try {
                     startActivityForResult(intent, 2);
-                }
-                catch (Exception e){
+                } catch (Exception e) {
                     textView.setText(e.toString());
-                    Toast.makeText(getApplicationContext(),e.toString(),Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
                 }
             }
         });
-        }
-        public void detect(Bitmap bitmap){
-            BitmapFactory.Options options = new BitmapFactory.Options();
-            options.inMutable=true;
-            Bitmap myBitmap = BitmapFactory.decodeResource(
-                    getApplicationContext().getResources(),
-                    R.drawable.ff,
-                    options);
-            myBitmap = bitmap;
-            Paint myRectPaint = new Paint();
-            myRectPaint.setStrokeWidth(5);
-            myRectPaint.setColor(Color.RED);
-            myRectPaint.setStyle(Paint.Style.STROKE);
+        object.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE);
 
-            Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
-            Canvas tempCanvas = new Canvas(tempBitmap);
-            tempCanvas.drawBitmap(myBitmap, 0, 0, null);
-
-            com.google.android.gms.vision.face.FaceDetector faceDetector = new
-                    FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false)
-                    .build();
-            if(!faceDetector.isOperational()){
-                new AlertDialog.Builder(getApplicationContext()).setMessage("Could not set up the face detector!").show();
-                return;
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                try {
+                    startActivityForResult(intent, 3);
+                } catch (Exception e) {
+                    textView.setText(e.toString());
+                    Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+                }
             }
-            Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
-            SparseArray<Face> faces = faceDetector.detect(frame);
+        });
+    }
 
-            for(int i=0; i<faces.size(); i++) {
-                Face thisFace = faces.valueAt(i);
-                float x1 = thisFace.getPosition().x;
-                float y1 = thisFace.getPosition().y;
-                float x2 = x1 + thisFace.getWidth();
-                float y2 = y1 + thisFace.getHeight();
-                tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
-            }
-            imageView.setImageDrawable(new BitmapDrawable(getResources(),tempBitmap));
+    public void detectFace(Bitmap bitmap) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inMutable = true;
+        Bitmap myBitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.ff,
+                options);
+        myBitmap = bitmap;
+        Paint myRectPaint = new Paint();
+        myRectPaint.setStrokeWidth(1);
+        myRectPaint.setColor(Color.RED);
+        myRectPaint.setStyle(Paint.Style.STROKE);
 
+        Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
+        Canvas tempCanvas = new Canvas(tempBitmap);
+        tempCanvas.drawBitmap(myBitmap, 0, 0, null);
+
+        com.google.android.gms.vision.face.FaceDetector faceDetector = new
+                FaceDetector.Builder(getApplicationContext()).setTrackingEnabled(false)
+                .build();
+        if (!faceDetector.isOperational()) {
+            new AlertDialog.Builder(getApplicationContext()).setMessage("Could not set up the face detector!").show();
+            return;
         }
+        Frame frame = new Frame.Builder().setBitmap(myBitmap).build();
+        SparseArray<Face> faces = faceDetector.detect(frame);
 
-        public void detectText(Bitmap bimage){
-            final Canvas canvas = new Canvas(bimage);
-            final Paint myRectPaint = new Paint();
-            myRectPaint.setStrokeWidth(5);
-            myRectPaint.setColor(Color.GREEN);
-            myRectPaint.setStyle(Paint.Style.STROKE);
+        for (int i = 0; i < faces.size(); i++) {
+            Face thisFace = faces.valueAt(i);
+            float x1 = thisFace.getPosition().x;
+            float y1 = thisFace.getPosition().y;
+            float x2 = x1 + thisFace.getWidth();
+            float y2 = y1 + thisFace.getHeight();
+            tempCanvas.drawRoundRect(new RectF(x1, y1, x2, y2), 2, 2, myRectPaint);
+        }
+        imageView.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
 
-
-            FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bimage);
-            FirebaseVisionTextRecognizer recognizer = FirebaseVision.getInstance()
-                    .getOnDeviceTextRecognizer();
-            //mTextButton.setEnabled(false);
-            recognizer.processImage(image)
-                    .addOnSuccessListener(
-                            new OnSuccessListener<FirebaseVisionText>() {
-                                @Override
-                                public void onSuccess(FirebaseVisionText texts) {
-                                    //mTextButton.setEnabled(true);
-                                    //processTextRecognitionResult(texts);
-                                    List<FirebaseVisionText.TextBlock> blocks = texts.getTextBlocks();
-                                    if (blocks.size() == 0) {
-                                        Toast.makeText(getApplicationContext(),"No Text found",Toast.LENGTH_LONG).show();
-                                        return;
-                                    }
+    }
+    public void detectText(final Bitmap myBitmap) {
+        final Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
+        Canvas tempCanvas = new Canvas(tempBitmap);
+        tempCanvas.drawBitmap(myBitmap, 0, 0, null);
+        FirebaseVisionTextRecognizer recognizer;
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(tempBitmap);
+        //recognizer = FirebaseVision.getInstance().getCloudTextRecognizer();
+        //if (recognizer==null)
+             recognizer = FirebaseVision.getInstance()
+                .getOnDeviceTextRecognizer();
+        //mTextButton.setEnabled(false);
+        recognizer.processImage(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText texts) {
+                                //mTextButton.setEnabled(true);
+                                //processTextRecognitionResult(texts);
+                                List<FirebaseVisionText.TextBlock> blocks = texts.getTextBlocks();
+                                if (blocks.size() == 0) {
+                                    Toast.makeText(getApplicationContext(), "No Text found", Toast.LENGTH_LONG).show();
+                                    return;
+                                }
+                                try {
+                                    Canvas canvas = new Canvas(tempBitmap);
+                                    Paint myRectPaint = new Paint();
+                                    myRectPaint.setStrokeWidth(1f);
+                                    myRectPaint.setColor(Color.GREEN);
+                                    myRectPaint.setStyle(Paint.Style.STROKE);
                                     //GraphicOverlay mGraphicOverlay = new GraphicOverlay(getApplicationContext(),null);
                                     //mGraphicOverlay.clear();
                                     for (int i = 0; i < blocks.size(); i++) {
@@ -161,28 +184,69 @@ public class MainActivity extends AppCompatActivity {
                                         for (int j = 0; j < lines.size(); j++) {
                                             List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
                                             Rect rect = blocks.get(i).getBoundingBox();
-                                            canvas.drawRect(rect,myRectPaint);
-                                            for (int k = 0; k < elements.size(); k++) {
+                                            canvas.drawRect(rect, myRectPaint);
+                                           /* for (int k = 0; k < elements.size(); k++) {
 
                                                 //GraphicOverlay.Graphic textGraphic = new TextGraphic(mGraphicOverlay, elements.get(k));
-                                               // mGraphicOverlay.add(textGraphic);
+                                                // mGraphicOverlay.add(textGraphic);
 
-                                            }
+                                            }*/
                                         }
                                     }
-                                }
-                            })
-                    .addOnFailureListener(
-                            new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    // Task failed with an exception
-                                    //mTextButton.setEnabled(true);
-                                    e.printStackTrace();
-                                }
-                            });
+                                    imageView.setImageDrawable(new BitmapDrawable(getResources(), tempBitmap));
+                                } catch (Exception e) {
+                                    textView.setText(e.toString());
 
-        }
+                                }
+
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                //mTextButton.setEnabled(true);
+                                textView.setText(e.toString());
+                            }
+                        });
+
+    }
+    public void detectObject(Bitmap myBitmap){
+        checkPermission(Manifest.permission.INTERNET,103);
+        FirebaseVisionImageLabeler labeler = FirebaseVision.getInstance()
+                .getOnDeviceImageLabeler();
+         /*final Bitmap tempBitmap = Bitmap.createBitmap(myBitmap.getWidth(), myBitmap.getHeight(), Bitmap.Config.RGB_565);
+         Canvas tempCanvas = new Canvas(tempBitmap);
+
+        tempCanvas.drawBitmap(myBitmap, 0, 0, null);*/
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(myBitmap);
+        labeler.processImage(image)
+                .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionImageLabel>>() {
+            @Override
+            public void onSuccess(List<FirebaseVisionImageLabel> labels) {
+                // Task completed successfully
+                // ...
+                textView.setText("");imageView.setVisibility(View.GONE);
+                  for (FirebaseVisionImageLabel label: labels) {
+                    String text = label.getText();
+                    String entityId = label.getEntityId();
+                    float confidence = label.getConfidence();
+                    textView.setText(textView.getText()+text+entityId+" "+confidence);
+                 }
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Task failed with an exception
+                        e.printStackTrace();
+                        textView.setText(e.toString());
+
+                    }
+                });
+
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -191,25 +255,29 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(getApplicationContext(), "in onActivityResult function", Toast.LENGTH_LONG).show();
             Bitmap photo = (Bitmap) data.getExtras().get("data");
             //imageView.setImageBitmap(photo);
-            if(requestCode==1)
+            switch (requestCode){
+                case 1:detectText(photo);break;
+                case 2:detectFace(photo);break;
+                case 3:detectObject(photo);break;
+            }
+            /*if (requestCode == 1)
                 detectText(photo);
             else
                 detect(photo);
-
+*/
         }
     }
+
     // Function to check and request permission.
-    public void checkPermission(String permission, int requestCode)
-    {
+    public void checkPermission(String permission, int requestCode) {
         if (ContextCompat.checkSelfPermission(MainActivity.this, permission)
                 == PackageManager.PERMISSION_DENIED) {
 
             // Requesting the permission
             ActivityCompat.requestPermissions(MainActivity.this,
-                    new String[] { permission },
+                    new String[]{permission},
                     requestCode);
-        }
-        else {
+        } else {
             Toast.makeText(MainActivity.this,
                     "Permission already granted",
                     Toast.LENGTH_SHORT)
@@ -224,8 +292,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String[] permissions,
-                                           @NonNull int[] grantResults)
-    {
+                                           @NonNull int[] grantResults) {
         super
                 .onRequestPermissionsResult(requestCode,
                         permissions,
@@ -238,23 +305,20 @@ public class MainActivity extends AppCompatActivity {
                         "Camera Permission Granted",
                         Toast.LENGTH_SHORT)
                         .show();
-            }
-            else {
+            } else {
                 Toast.makeText(MainActivity.this,
                         "Camera Permission Denied",
                         Toast.LENGTH_SHORT)
                         .show();
             }
-        }
-        else if (requestCode == STORAGE_PERMISSION_CODE) {
+        } else if (requestCode == STORAGE_PERMISSION_CODE) {
             if (grantResults.length > 0
                     && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(MainActivity.this,
                         "Storage Permission Granted",
                         Toast.LENGTH_SHORT)
                         .show();
-            }
-            else {
+            } else {
                 Toast.makeText(MainActivity.this,
                         "Storage Permission Denied",
                         Toast.LENGTH_SHORT)
